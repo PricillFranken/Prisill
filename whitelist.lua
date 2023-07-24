b77f768e62b69f3d7beb3ea811c4690a6b478baa
PricillFranken <108398406+PricillFranken@users.noreply.github.com>
2023-07-24 13:14:54 +0300

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,7 +1,7 @@
 local whitelistecheck = loadstring(game:HttpGet("https://raw.githubusercontent.com/PricillFranken/Prisill/main/data.lua", true))()
 
 if whitelistecheck[game:service('Players').LocalPlayer.UserId] then
- loadstring(game:HttpGet("https://raw.githubusercontent.com/PricillFranken/UIRB/main/UI"))()
+ loadstring(game:HttpGet("https://pastebin.com/raw/3kY9iVDD"))()
 else
   game:service('Players').LocalPlayer:Kick('You have been banned from Roblox')
 end
