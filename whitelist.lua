a13525e3e875b17133e8791da5caed7e86b353e6
PricillFranken <108398406+PricillFranken@users.noreply.github.com>
2023-07-24 13:25:15 +0300

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,5 +1,5 @@
 local whitelistecheck = loadstring(game:HttpGet("https://raw.githubusercontent.com/PricillFranken/Prisill/main/data.lua", true))()
-local gui = loadstring(game:HttpGet("https://raw.githubusercontent.com/PricillFranken/Prisill/main/UI/Flux%20Lib%20Example.lua", true))()
+local gui = loadstring(game:HttpGet("https://raw.githubusercontent.com/PricillFranken/Prisill/main/UI/Flux%20Lib%20Example.lua"))()
 if whitelistecheck[game:service('Players').LocalPlayer.UserId] then
  gui == true
 else
